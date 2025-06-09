3072c1b62c5dc7fa0761f498c718fb4379fd1d38
KitsuneHeartsFM <kitsuneheartsfinalmix@gmail.com>
2025-06-09 16:08:56 -0300

    2

---
+++ Pilha/PilhaEstatica/PilhaEstatica/IEmpilhavel.cs
@@ -0,0 +1,14 @@
+namespace PilhaEstatica;
+
+public interface IEmpilhavel <T>
+{
+    // métodos principais
+    void Empilhar(T dado);
+    T Espiar();
+    T Desempilhar();
+    
+    // métodos auxiliares
+    bool EstaCheia();
+    bool EstaVazia();
+    string Imprimir();
+}
